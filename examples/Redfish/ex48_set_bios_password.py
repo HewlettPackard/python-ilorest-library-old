44fbdf5affa3ecbd1c49718f04d336eaea9cae00
Jack Garcia <rexysmydog@gmail.com>
2016-09-06 17:15:11 -0500

    adding new bios password example

---
+++ examples/Redfish/ex48_set_bios_password.py
@@ -0,0 +1,55 @@
+ # Copyright 2016 Hewlett Packard Enterprise Development LP
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License"); you may
+ # not use this file except in compliance with the License. You may obtain
+ # a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ # License for the specific language governing permissions and limitations
+ # under the License.
+
+import sys
+from _redfishobject import RedfishObject
+from ilorest.rest.v1_helper import ServerDownOrUnreachableError
+
+def ex48_set_bios_password(redfishobj, new_password, bios_password=None):
+    sys.stdout.write("\nEXAMPLE 48: Set Bios Password\n")
+    instances = redfishobj.search_for_type("ComputerSystem.")
+
+    for instance in instances:
+        body = {"AdminPassword": new_password, \
+                "OldAdminPassword": bios_password}
+        response = redfishobj.redfish_patch(instance["@odata.id"], body, \
+                                            optionalpassword=bios_password)
+        redfishobj.error_handler(response)
+
+if __name__ == "__main__":
+    # When running on the server locally use the following commented values
+    # iLO_https_url = "blobstore://."
+    # iLO_account = "None"
+    # iLO_password = "None"
+
+    # When running remotely connect using the iLO secured (https://) address, 
+    # iLO account name, and password to send https requests
+    # iLO_https_url acceptable examples:
+    # "https://10.0.0.100"
+    # "https://f250asha.americas.hpqcorp.net"
+    iLO_https_url = "https://10.0.0.100"
+    iLO_account = "admin"
+    iLO_password = "password"
+
+    # Create a REDFISH object
+    try:
+        REDFISH_OBJ = RedfishObject(iLO_https_url, iLO_account, iLO_password)
+    except ServerDownOrUnreachableError, excp:
+        sys.stderr.write("ERROR: server not reachable or doesn't support " \
+                                                                "RedFish.\n")
+        sys.exit()
+    except Exception, excp:
+        raise excp
+
+    ex48_set_bios_password(REDFISH_OBJ, "newpassword", "biospassword")
---
+++ examples/Rest/ex48_set_bios_password.py
@@ -0,0 +1,47 @@
+# Copyright 2016 Hewlett Packard Enterprise Development, LP.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License"); you may
+ # not use this file except in compliance with the License. You may obtain
+ # a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ # License for the specific language governing permissions and limitations
+ # under the License.
+
+import sys
+from _restobject import RestObject
+
+def ex48_set_bios_password(restobj, new_password, bios_password=None):
+    sys.stdout.write("\nEXAMPLE 48: Set Bios Password\n")
+    instances = restobj.search_for_type("Bios.")
+
+    for instance in instances:
+        body = {"AdminPassword": new_password, \
+                "OldAdminPassword": bios_password}
+        response = restobj.rest_patch(instance["href"], body, \
+                                      bios_password)
+        restobj.error_handler(response)
+
+if __name__ == "__main__":
+    # When running on the server locally use the following commented values
+    # iLO_https_url = "blobstore://."
+    # iLO_account = "None"
+    # iLO_password = "None"
+
+    # When running remotely connect using the iLO secured (https://) address, 
+    # iLO account name, and password to send https requests
+    # iLO_https_url acceptable examples:
+    # "https://10.0.0.100"
+    # "https://f250asha.americas.hpqcorp.net"
+    iLO_https_url = "https://10.0.0.100"
+    iLO_account = "admin"
+    iLO_password = "password"
+    
+    #Create a REST object
+    REST_OBJ = RestObject(iLO_https_url, iLO_account, iLO_password)
+    ex48_set_bios_password(REST_OBJ, "newpassword", "biospassword")
+ 
